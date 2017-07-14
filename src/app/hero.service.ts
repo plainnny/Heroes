d2c461fe211d66d517b7748330ad670a826cbaea
kny <nymsc@mscbrain.com>
2017-07-14 19:19:56 +0900

    commit3

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,60 +1,12 @@
 import { Injectable } from '@angular/core';
-<<<<<<< HEAD
-import { Headers, Http } from '@angular/http';
-import 'rxjs/add/operator/toPromise';
-
-import { Hero } from './hero';
-=======
 
 import  { Hero } from './hero';
 import { HEROES } from './mock-heroes';
->>>>>>> commit
 
 @Injectable()
 export class HeroService {
 
-<<<<<<< HEAD
-  private heroesUrl = 'api/heroes';  // URL to weh api
-  private headers = new Headers({'Content-Type': 'application/json'});
-
-    constructor(private http: Http) { }
-
-  getHeroes(): Promise<Hero[]> {
-    return this.http.get(this.heroesUrl)
-                .toPromise()
-                .then(response => response.json().data as Hero[])
-                .catch(this.handleError);
-  }
-
-  getHeroesSlowly(): Promise<Hero[]> {
-    return new Promise(resolve => {
-      setTimeout(() => resolve(this.getHeroes()), 2000);
-    });
-  }
-
-  getHero(id: number): Promise<Hero> {
-    const url = `${this.heroesUrl}/${id}`;
-    return this.http.get(url)
-                .toPromise()
-                .then(response => response.json().data as Hero)
-                .catch(this.handleError);
-    }
-
-  update(hero: Hero): Promise<Hero> {
-    const url = `${this.heroesUrl}/${hero.id}`;
-    return this.http
-      .put(url, JSON.stringify(hero), {headers: this.headers})
-      .toPromise()
-      .then(() => hero)
-      .catch(this.handleError);
-  }
-
-  private handleError(error: any): Promise<any> {
-    console.error('An error occurred', error);
-    return Promise.reject(error.message || error);
-=======
   getHeroes(): Hero[] {
     return HEROES;
->>>>>>> commit
   }
 }
