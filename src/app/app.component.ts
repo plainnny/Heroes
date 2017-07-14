d1594c5f7e965e754fcce86c21698ebbdd01102d
kny <nymsc@mscbrain.com>
2017-07-14 19:18:31 +0900

    commit2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [HeroService]
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Tour of Heroes';
+  heroes: Hero[];
+  selectedHero: Hero;
+
+  constructor(private heroService: HeroService) {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes()
+  };
+
+  onSelect(hero: Hero): void {
+
+    this.selectedHero = hero;
+  }
 }
